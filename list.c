e7665034b5cfc50328fd8839bba154341651207c
richard <richard>
2002-04-28 23:21:03 +0000

    Add support for limiting depth of entries that are displayed.

--- list.c
+++ list.c
@@ -1,8 +1,8 @@
 /*
-   $Header: /cvs/src/tdl/list.c,v 1.8 2001/11/11 22:34:03 richard Exp $
+   $Header: /cvs/src/tdl/list.c,v 1.9 2002/04/28 23:21:03 richard Exp $
   
    tdl - A console program for managing to-do lists
-   Copyright (C) 2001  Richard P. Curnow
+   Copyright (C) 2001,2002  Richard P. Curnow
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,6 +23,14 @@
 #include <string.h>
 #include <ctype.h>
 #include "tdl.h"
+
+struct list_options {
+  unsigned monochrome:1;
+  unsigned show_all:1;
+  unsigned verbose:1;
+  unsigned set_depth:1;
+  int depth;
+};
 
 #define INDENT_TAB 3
 
@@ -89,24 +97,58 @@ static void print_timestamp(int timestamp, char *leader, int indent, int monochr
   }
 }
 /*}}}*/
-static void print_details(struct node *y, int indent, int verbose, int all, int monochrome, char *index_buffer, time_t now)/*{{{*/
+static void count_kids(struct links *x, int *n_kids, int *n_done_kids, int *n_open_kids)/*{{{*/
+{
+  int nk, ndk;
+  struct node *y;
+
+  nk = ndk = 0;
+  for (y = x->next;
+       y != (struct node *) x;
+       y = y->chain.next) {
+    if (y->done) ndk++;
+    nk++;
+  }
+
+  if (n_kids) *n_kids = nk;
+  if (n_done_kids) *n_done_kids = ndk;
+  if (n_open_kids) *n_open_kids = (nk - ndk);
+  return;
+}
+/*}}}*/
+static void print_details(struct node *y, int indent, int summarise_kids, const struct list_options *options, char *index_buffer, time_t now)/*{{{*/
 {
   int is_done;
   char *p;
+  int n_kids, n_open_kids;
 
   is_done = (y->done > 0);
-  if (!all && is_done) return;
+  if (!options->show_all && is_done) return;
 
   do_indent(indent);
-  if (monochrome) {
-    printf("%s %s", index_buffer,
-           (all && !verbose && is_done) ? "(DONE) : " : 
-           (all && !verbose && (y->arrived > now)) ? "(DEFERRED) : " : ": ");
+  count_kids(&y->kids, &n_kids, NULL, &n_open_kids);
+  if (summarise_kids && (n_kids > 0)) {
+    if (options->monochrome) {
+      printf("%s [%d/%d] %s", index_buffer, n_open_kids, n_kids,
+             (options->show_all && !options->verbose && is_done) ? "(DONE) : " : 
+             (options->show_all && !options->verbose && (y->arrived > now)) ? "(DEFERRED) : " : ": ");
+    } else {
+      printf("%s%s %s[%d/%d]%s %s%s", GREEN, index_buffer, YELLOW, n_open_kids, n_kids, NORMAL,
+             (options->show_all && !options->verbose && is_done) ? CYAN "(DONE) " NORMAL :
+             (options->show_all && !options->verbose && (y->arrived > now)) ? MAGENTA "(DEFERRED) " : "",
+             is_done ? DIMCYAN : colour_table[y->priority]);
+    }
   } else {
-    printf("%s%s%s %s%s", GREEN, index_buffer, NORMAL,
-           (all && !verbose && is_done) ? CYAN "(DONE) " NORMAL :
-           (all && !verbose && (y->arrived > now)) ? MAGENTA "(DEFERRED) " : "",
-           is_done ? DIMCYAN : colour_table[y->priority]);
+    if (options->monochrome) {
+      printf("%s %s", index_buffer,
+             (options->show_all && !options->verbose && is_done) ? "(DONE) : " : 
+             (options->show_all && !options->verbose && (y->arrived > now)) ? "(DEFERRED) : " : ": ");
+    } else {
+      printf("%s%s%s %s%s", GREEN, index_buffer, NORMAL,
+             (options->show_all && !options->verbose && is_done) ? CYAN "(DONE) " NORMAL :
+             (options->show_all && !options->verbose && (y->arrived > now)) ? MAGENTA "(DEFERRED) " : "",
+             is_done ? DIMCYAN : colour_table[y->priority]);
+    }
   }
   for (p = y->text; *p; p++) {
     putchar(*p);
@@ -114,25 +156,25 @@ static void print_details(struct node *y, int indent, int verbose, int all, int 
       do_indent(indent + 5);
     }
   }
-  if (!monochrome) printf("%s", NORMAL);
+  if (!options->monochrome) printf("%s", NORMAL);
   printf("\n");
-  if (verbose) {
-    print_timestamp(y->arrived, "Arrived", indent, monochrome);
+  if (options->verbose) {
+    print_timestamp(y->arrived, "Arrived", indent, options->monochrome);
     do_indent(indent + 2);
-    if (monochrome) {
+    if (options->monochrome) {
       printf("Priority: %s\n", priority_text[y->priority]);
     } else {
       printf("%sPriority: %s%s%s\n",
           GREEN, colour_table[y->priority], priority_text[y->priority], NORMAL);
     }
-    if (y->required_by > 0) print_timestamp(y->required_by, "Required by", indent, monochrome);
-    if (y->done > 0) print_timestamp(y->done, "Completed", indent, monochrome);
+    if (y->required_by > 0) print_timestamp(y->required_by, "Required by", indent, options->monochrome);
+    if (y->done > 0) print_timestamp(y->done, "Completed", indent, options->monochrome);
     printf("\n");
   }
 
 }
 /*}}}*/
-static void list_chain(struct links *x, int indent, int verbose, int all, int monochrome, char *index_buffer, enum Priority prio, time_t now)/*{{{*/
+static void list_chain(struct links *x, int indent, int depth, const struct list_options *options, char *index_buffer, enum Priority prio, time_t now)/*{{{*/
 {
   struct node *y;
   int idx, is_done;
@@ -146,7 +188,7 @@ static void list_chain(struct links *x, int indent, int verbose, int all, int mo
     is_done = (y->done > 0);
     /* Normally, don't show entries that have been done, or whose start time
      * has been deferred */
-    if (!all && (is_done || (y->arrived > now))) continue;
+    if (!options->show_all && (is_done || (y->arrived > now))) continue;
 
     sprintf(component_buffer, "%d", idx);
     strcpy(new_index_buffer, index_buffer);
@@ -156,11 +198,14 @@ static void list_chain(struct links *x, int indent, int verbose, int all, int mo
     strcat(new_index_buffer, component_buffer);
 
     if (y->priority >= prio) {
-      print_details(y, indent, verbose, all, monochrome, new_index_buffer, now);
+      int summarise_kids = (options->set_depth && (options->depth == depth));
+      print_details(y, indent, summarise_kids, options, new_index_buffer, now);
     }
 
     /* Maybe list children regardless of priority assigned to parent. */
-    list_chain(&y->kids, indent + INDENT_TAB, verbose, all, monochrome, new_index_buffer, prio, now);
+    if (!options->set_depth || (depth < options->depth)) {
+      list_chain(&y->kids, indent + INDENT_TAB, depth + 1, options, new_index_buffer, prio, now);
+    }
 
   }
   return;
@@ -168,10 +213,9 @@ static void list_chain(struct links *x, int indent, int verbose, int all, int mo
 /*}}}*/
 void process_list(char **x)/*{{{*/
 {
-  int verbose = 0;
-  int all = 0; /* Even show done nodes */
+  struct list_options options;
+  int options_done = 0;
   int any_paths = 0;
-  int monochrome = 0;
   char index_buffer[64];
   char *y;
   enum Priority prio = PRI_NORMAL, prio_to_use, node_prio;
@@ -179,29 +223,64 @@ void process_list(char **x)/*{{{*/
   time_t now = time(NULL);
 
   if (getenv("TDL_LIST_MONOCHROME") != 0) {
-    monochrome = 1;
+    options.monochrome = 1;
   }
+
+  options.monochrome = 0;
+  options.show_all = 0;
+  options.verbose = 0;
+  options.set_depth = 0;
   
   while ((y = *x) != 0) {
-    if (!strcmp(y, "-v")) verbose = 1;
-    else if (!strcmp(y, "-a")) all = 1;
-    else if (!strcmp(y, "-m")) monochrome = 1;
-    else if (isdigit(y[0]) ||
-             (((y[0] == '-') || (y[0] == '+')) &&
-              (isdigit(y[1])))) {
+    /* An argument starting '1' or '+1' or '+-1' (or '-1' after '--') is
+     * treated as the path of the top node to show */
+    if (isdigit(y[0]) ||
+        (options_done && (y[0] == '-') && isdigit(y[1])) ||
+        ((y[0] == '+') &&
+         (isdigit(y[1]) ||
+          ((y[1] == '-' && isdigit(y[2])))))) {
       
       struct node *n = lookup_node(y, 0, NULL);
+      int summarise_kids;
       any_paths = 1;
       index_buffer[0] = '\0';
       strcpy(index_buffer, y);
-      print_details(n, 0, verbose, all, monochrome, index_buffer, now);
-      node_prio = n->priority;
+      summarise_kids = (options.set_depth && (options.depth==0));
+      print_details(n, 0, summarise_kids, &options, index_buffer, now);
+      if (!options.set_depth || (options.depth > 0)) {
+        node_prio = n->priority;
 
-      /* If the priority has been set on the cmd line, always use that.
-       * Otherwise, use the priority from the specified node, _except_ when
-       * that is higher than normal, in which case use normal. */
-      prio_to_use = (prio_set) ? prio : ((node_prio > prio) ? prio : node_prio);
-      list_chain(&n->kids, INDENT_TAB, verbose, all, monochrome, index_buffer, prio_to_use, now);
+        /* If the priority has been set on the cmd line, always use that.
+         * Otherwise, use the priority from the specified node, _except_ when
+         * that is higher than normal, in which case use normal. */
+        prio_to_use = (prio_set) ? prio : ((node_prio > prio) ? prio : node_prio);
+        list_chain(&n->kids, INDENT_TAB, 0, &options, index_buffer, prio_to_use, now);
+      }
+    } else if ((y[0] == '-') && (y[1] == '-')) {
+      options_done = 1;
+    } else if (y[0] == '-') {
+      while (*++y) {
+        switch (*y) {
+          case 'v':
+            options.verbose = 1;
+            break;
+          case 'a':
+            options.show_all = 1;
+            break;
+          case 'm':
+            options.monochrome = 1;
+            break;
+          case '1': case '2': case '3':
+          case '4': case '5': case '6': 
+          case '7': case '8': case '9':
+            options.set_depth = 1;
+            options.depth = (*y) - '1';
+            break;
+          default:
+            fprintf(stderr, "Unrecognized option : -%c\n", *y);
+            break;
+        }
+      }
     } else {
       prio = parse_priority(y);
       prio_set = 1;
@@ -212,7 +291,7 @@ void process_list(char **x)/*{{{*/
   
   if (!any_paths) {
     index_buffer[0] = 0;
-    list_chain(&top, 0, verbose, all, monochrome, index_buffer, prio, now);
+    list_chain(&top, 0, 0, &options, index_buffer, prio, now);
   }
 }
 /*}}}*/
